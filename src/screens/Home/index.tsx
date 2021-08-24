0dc7d421d9c4837c72135889b3a42367f8cf8db6
Gustavo <rochatambelini@gmail.com>
2021-08-24 07:26:56 -0300

    Criação do projeto e dica ninja

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+    Container,
+    Title
+} from './style';
+
+export function Home(){
+    return(
+        <Container>
+            <Title> HOME </Title>
+        </Container>
+    )
+}
